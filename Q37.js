9d83714d18a57b0dc6c254a70853b0612cc9d2a0
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-23 19:55:40 -0700

    Question is solved

---
+++ Q37.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countDistinctIntegers = function(nums) {
+    let set = new Set(); // Step 1: Create a set to keep unique numbers.
+    
+    for (let num of nums) {
+        set.add(num); // Step 2: Add the number itself.
+        
+        // Reverse the number:
+        let reversedNum = parseInt(num.toString().split('').reverse().join(''));
+        set.add(reversedNum); // Step 3: Add the reversed number.
+    }
+    
+    return set.size; // Step 4: Return how many unique numbers there are.
+};
+console.log(countDistinctIntegers([ 1,13,10,12,31]))
