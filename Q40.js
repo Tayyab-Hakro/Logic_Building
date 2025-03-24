c0c959432454ac66c435062c1ba5d478ed167593
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-24 03:51:16 -0700

    24/3/24

---
+++ Q40.js
@@ -0,0 +1,21 @@
+var removeOuterParentheses = function(s) 
+{
+    let ans = ''
+    let cnt = 0
+    for (ch of s)
+    {
+        if(ch === '(')
+        {
+            if(cnt) ans += ch;
+            cnt++;
+        }
+        else 
+        {
+            cnt--;
+            if(cnt) ans += ch;
+        }
+    }
+    return ans
+};
+
+console.log(removeOuterParentheses("(()())(())"))
