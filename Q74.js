64c25041d4c21046a769e56516459376a14c318f
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-31 06:26:36 -0700

    Woerking on eid day

--- Q74.js
+++ Q74.js
@@ -1,19 +1,40 @@
-var countKDifference = function(nums, k) {
-    let map = {};
-    for(let i of nums) {
-        map[i] ? map[i]++ : map[i] = 1 
+/**
+ * Finds the first palindromic string in the given array of words.
+ * @param {string[]} words - Array of words
+ * @returns {string} - First palindromic string found, or an empty string if none found
+ */
+const firstPalindrome = function(words) {
+    // Iterate through each word in the array
+    for (const word of words) {
+      // Check if the current word is a palindrome
+      if (isPalindromic(word)) {
+        // If it is a palindrome, return it immediately
+        return word;
+      }
     }
-        console.log(map)
-    let pairs = 0;
-    
-    for(let i = 0; i<nums.length; i++) {
-        if(map[nums[i]-k]) {
-            pairs += map[nums[i] - k]
-        }
+    // If no palindrome found, return an empty string
+    return "";
+  };
+  
+  /**
+   * Checks if the given string is a palindrome.
+   * @param {string} str - The string to check
+   * @returns {boolean} - True if the string is a palindrome, false otherwise
+   */
+  function isPalindromic(str) {
+    let start = 0; // Pointer starting from the beginning of the string
+    let end = str.length - 1; // Pointer starting from the end of the string
+  
+    // Iterate until start pointer is less than end pointer
+    while (start < end) {
+      // If characters at start and end pointers don't match, it's not a palindrome
+      if (str[start] !== str[end]) {
+        return false;
+      }
+      // Move start pointer forward and end pointer backward
+      start++;
+      end--;
     }
-    
-    return pairs;
-    
-};
-
-console.log( countKDifference([1,2,2,1]), 1)
+    // If the loop completes without returning false, the string is a palindrome
+    return true;
+  }
