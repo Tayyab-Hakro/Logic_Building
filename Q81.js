544804d5761fb19bf777474c3679b3498f28c02f
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-04-01 13:36:52 -0700

    wORKING ON question

--- Q81.js
+++ Q81.js
@@ -1,2 +1,7 @@
-let arr = 'd'
-console.log( String.fromCharCode(0))
+var addedInteger = function(nums1, nums2) {
+nums1 = nums1.sort((a , b) => a-b)
+nums2 = nums2.sort((a , b) => a-b)
+let mapped = nums2.map((item,index) => item-nums1[index] )
+return mapped[0]
+}
+console.log(addedInteger([2,6,4], [9,7,5]))
